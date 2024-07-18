91ae96f16640b839ab9eb6d1f4b248a5602a4fc0
Thejavro1 <vietnamserver909d@gmail.com>
2024-07-19 00:08:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
-PICS = (environ.get('PICS', 'https://graph.org/file/d16399222df7af9aa1e3b.mp4')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://graph.org/file/6dd13b911cef0033f21a2.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/b69af2db776e4e85d21ec.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Open_Linkl")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
